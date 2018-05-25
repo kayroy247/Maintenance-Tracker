21acc045c88afdc4ee8b2627e370cb2f7e67d4a9
Kayode Okunlade <okunladekayode@gmail.com>
2018-05-25 16:46:12 -0700

    mocha test

--- test/appTest.js
+++ test/appTest.js
@@ -3,44 +3,16 @@ import chai from 'chai';
 import myapp from '../routers/repairRequestRoutes';
 import request from 'request';
 const { expect } = chai.expect;
-
+const should = chai.should();
 
 
 describe('Maintenance-Tracker Route Tests', () => {
   
     it('Should fetch all the request of a logged in user', function (done) {
-      request('http://localhost:5000', (error, response, body) => {
-         expect(response.statusCode).to.equal(200);
-         done();
+      request('/', (err, res) => {
+      res.body.should.have.status(200);
+    });
     });
 });
   
-  it('Should fetch a request that belong to a user', function (done) {
-    request('http://localhost:5000', (error, response, body) => {
-      expect(response.statusCode).to.equal(200);
-      done();
-  });
-  });
-
-  it('should test non existent id by returning 404', function (done) {
-    request('http://localhost:5000', (error, response, body) => {
-         expect(response.statusCode).to.equal(404);
-         done();
-  });
-});
-describe('POST /users/requests', () => {
-  const newRequest = {
-     name: 'Frank jeff',
-      userId : 3,
-      title: 'Car repair',
-      description: 'The car has engine fault',
-  }
-  it('should Modify a request', (done) => {
-    request('http://localhost:5000', (error, response, body) => {
-         expect(response.statusCode).to.equal(200);
-         done();
-  });
-});
-});
-});
-
+ 
